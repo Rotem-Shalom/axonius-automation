1fd9fe44f993a4f991f66145bd8cd458bf1b063c
Rotem Shalom <rotems4922@gmail.com>
2025-05-27 14:10:45 +0300

    remove class

--- tests/airbnb_tests.py
+++ tests/airbnb_tests.py
@@ -11,31 +11,32 @@ from utils.test_utils import generate_search_with_random_dates, open_item_in_new
 logging.basicConfig(level=logging.INFO)
 
 
-class TestAirbnb:
-    @pytest.fixture
-    def setup_search(self, page):
-        search_page = SearchPage(page)
-        search_page.go_to(consts.AIRBNB_WEB_ADDRESS)
-        generate_search_with_random_dates(search_page=search_page, location=consts.TEST_CITY,
-                                          adults_count=2, children_count=0)
-        return ResultsPage(page)
+@pytest.fixture
+def setup_search(page):
+    search_page = SearchPage(page)
+    search_page.go_to(consts.AIRBNB_WEB_ADDRESS)
+    generate_search_with_random_dates(search_page=search_page, location=consts.TEST_CITY,
+                                      adults_count=2, children_count=0)
+    return ResultsPage(page)
 
-    def test_validate_search_and_get_highest_and_cheapest_items(self, setup_search):
-        results_page = setup_search
-        assert consts.TEST_CITY in results_page.get_page_heading_text()
-        items = results_page.get_all_items()
-        logging.info(f"Highest Rated Item: {results_page.find_highest_rated_item(items)}")
-        logging.info(f"Cheapest Item: {results_page.find_cheapest_item(items)}")
 
-    def test_validate_search_and_reservation_details(self, setup_search, page):
-        results_page = setup_search
-        assert consts.TEST_CITY in results_page.get_page_heading_text()
-        highest_rate = results_page.find_highest_rated_item(results_page.get_all_items())
-        reservation_page = ReservationPage(open_item_in_new_tab(page=page, item=highest_rate))
-        reservation_page.close_translation_window()
-        reservation_details = reservation_page.get_reservation_details()
-        reservation_details.print_details()
-        confirm_page = reservation_page.enter_on_reserve()
-        confirm_reservation_details = confirm_page.get_confirm_reservation_details()
-        assert reservation_details == confirm_reservation_details
-        confirm_page.enter_phone_number('546876567')
+def test_validate_search_and_get_highest_and_cheapest_items(setup_search):
+    results_page = setup_search
+    assert consts.TEST_CITY in results_page.get_page_heading_text()
+    items = results_page.get_all_items()
+    logging.info(f"Highest Rated Item: {results_page.find_highest_rated_item(items)}")
+    logging.info(f"Cheapest Item: {results_page.find_cheapest_item(items)}")
+
+
+def test_validate_search_and_reservation_details(setup_search, page):
+    results_page = setup_search
+    assert consts.TEST_CITY in results_page.get_page_heading_text()
+    highest_rate = results_page.find_highest_rated_item(results_page.get_all_items())
+    reservation_page = ReservationPage(open_item_in_new_tab(page=page, item=highest_rate))
+    reservation_page.close_translation_window()
+    reservation_details = reservation_page.get_reservation_details()
+    reservation_details.print_details()
+    confirm_page = reservation_page.enter_on_reserve()
+    confirm_reservation_details = confirm_page.get_confirm_reservation_details()
+    assert reservation_details == confirm_reservation_details
+    confirm_page.enter_phone_number('546876567')
